d54e60db8fb02fc022fd99d893226608d0bdd7e9
octo <dlqhdgkr456@gmail.com>
2022-02-12 15:33:49 +0900

    순수 JDBC

--- src/main/java/management/membermanagement/SpringConfig.java
+++ src/main/java/management/membermanagement/SpringConfig.java
@@ -1,13 +1,24 @@
 package management.membermanagement;
 
+import management.membermanagement.repository.JDBCMemberRepository;
 import management.membermanagement.repository.MemberRepository;
 import management.membermanagement.repository.MemoryMemberRepository;
 import management.membermanagement.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JDBCMemberRepository(dataSource);
     }
 }
