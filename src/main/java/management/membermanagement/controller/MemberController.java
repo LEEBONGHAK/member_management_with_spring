e5fdaa3ea1f2619e00f9060fbc4c3da7485f87b4
octo <dlqhdgkr456@gmail.com>
2022-02-12 14:04:13 +0900

    회원 웹 기능 - 홈 화면 추가

--- src/main/java/management/membermanagement/controller/MemberController.java
+++ src/main/java/management/membermanagement/controller/MemberController.java
@@ -3,6 +3,7 @@ package management.membermanagement.controller;
 import management.membermanagement.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
